b7ac94a25461d8114a8decb2726ab3b7512e996e
Alexander KIRILOV <alexander.n.kirilov@gmail.com>
2017-03-07 22:26:25 +0100

    Added default test

--- spring-boot-db/src/test/java/bg/alexander/SpringBootDbApplicationTests.java
+++ spring-boot-db/src/test/java/bg/alexander/SpringBootDbApplicationTests.java
@@ -1,16 +1,28 @@
 package bg.alexander;
 
+import bg.alexander.model.Model;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static junit.framework.TestCase.assertEquals;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootDbApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	public void contextLoads() {
+		ResponseEntity<Model> model = restTemplate.getForEntity("/model/1", Model.class);
+
+		assertEquals(model.getBody().getName(),"Alex");
 	}
 
 }
